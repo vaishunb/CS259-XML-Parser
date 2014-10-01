0a04d17a3498ffd871f62f50716b629509704c92
Aliaksei Yeusiukou <xRoker@users.noreply.github.com>
2014-10-01 23:57:17 +0300

    Task 22 - Done!
    …to write a Java program called AttributeConverter that takes as input
    an input XML file and uses JAXP 1.3’s SAX API to convert all attributes
    to child elements and serialize the result to System.out.6
    For instance, given
        <foo><bar baz="qux" quux="quuux"/></foo>
    as input, your program should produce
        <?xml version="1.0" encoding="UTF-8"?>
        <foo><bar><baz>qux</baz><quux>quuux</quux></bar></foo>

--- src/cscie259/project1/AttributeConverter.java
+++ src/cscie259/project1/AttributeConverter.java
@@ -1,7 +1,15 @@
 package cscie259.project1;
 
+import java.io.IOException;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 
 /**
@@ -14,8 +22,24 @@ import org.apache.xml.serialize.XMLSerializer;
  *
  * @author  Aliaksei Yeusiukou
  **/
-public class AttributeConverter
+public class AttributeConverter extends DefaultHandler
 {
+    /**
+     * An instance of XMLSerializer for writing
+     * to standard out
+     */
+	private XMLSerializer xmlSerializer;
+	
+	public AttributeConverter(XMLSerializer serializer){
+		xmlSerializer = serializer;
+		try {
+			xmlSerializer.asDocumentHandler();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
     /**
      * Main entry point to program.
      *
@@ -38,6 +62,55 @@ public class AttributeConverter
                 System.out,
                 new OutputFormat("XML", "UTF-8", true));
 
-        // TODO
+        SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        
+        SAXParser parser = null;
+        try {
+			parser = parserFactory.newSAXParser();
+			parser.parse(filename, new AttributeConverter(serializer));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+               
+        
     }
+
+	
+	/**
+     * Add converting to the overrided method
+     *
+     * @param uri - the namespace uri
+     * @param localName - the local name
+     * @param qName - the qualified name
+     * @param attributes - the attributes attached to the element
+     */
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		xmlSerializer.startElement(uri, localName, qName, null);
+		
+		for(int i=0; i<attributes.getLength(); i++){
+			String attributeName = attributes.getQName(i);
+			String attributeValue = attributes.getValue(i);
+			
+			xmlSerializer.startElement(attributeName, null);
+			xmlSerializer.characters(attributeValue.toCharArray(), 0, attributeValue.length());
+			xmlSerializer.endElement(attributeName);
+		}
+	}
+	
+	// Just redirect methods calls to our xmlserializer
+	@Override
+	public void endElement(String arg0, String arg1, String arg2) throws SAXException {
+		xmlSerializer.endElement(arg0, arg1, arg2);
+	}
+		
+
+	@Override
+	public void characters(char[] arg0, int arg1, int arg2) throws SAXException {
+		xmlSerializer.characters(arg0, arg1, arg2);
+	}
+    
+    
 }
